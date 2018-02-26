5aef20095ce8622cc92cc5d5b66fcdf89334e579
Leif Linder <leif@leiflinder.com>
2018-02-26 20:59:37 +0100

    after assign 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,34 @@
+// LEIF LINDER 2018-02-23
+// OSLO, NORWAY
+
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import 'hammerjs';
+
+// components
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
-import { LeaderService } from './services/leader.service';
-
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
-import { AppRoutingModule } from './app-routing/app-routing.module';
+// services
+/*
+import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
+*/
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+
 
 @NgModule({
   declarations: [
@@ -36,11 +43,12 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   ],
     imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService, LeaderService],
+  providers: [DishService, PromotionService,LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
