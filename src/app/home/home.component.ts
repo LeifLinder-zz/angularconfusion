3e9d13ba26eddbc3b6b847748ef66b780c8f88b8
Leif Linder <leif@leiflinder.com>
2018-06-19 19:32:03 +0200

    build

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,11 +29,14 @@ export class HomeComponent implements OnInit {
 
 dish: Dish;
 promotion: Promotion;
+errMess: string;
 leader: Leader;
 dishErrMess: string;
+leaderErrMess: string;
+promotionErrMess: string;
 
 constructor(private dishservice: DishService,
-private promotionservice: PromotionService, 
+private promotionservice: PromotionService,
 private leaderservice: LeaderService,
 @Inject('BaseURL') private BaseURL) { }
 
@@ -47,14 +50,22 @@ this.dishservice.getFeaturedDish().subscribe(dish => this.dish = dish,
 
 // this.promotion = this.promotionservice.getFeaturedPromotion();
 this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion,
-    errmess => this.dishErrMess = <any>errmess);
+    errmess => this.promotionErrMess = <any>errmess);
 // this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion);
 
 
 // this.leader = this.leaderservice.getFeaturedLeader();
 this.leaderservice.getFeaturedLeader().subscribe(leader => this.leader = leader,
-    errmess => this.dishErrMess = <any>errmess);
+    errmess => this.leaderErrMess = <any>errmess);
 // this.leaderservice.getFeaturedLeader().subscribe(leader => this.leader = leader);
+
+/*
+  ngOnInit() {
+    this.dishservice.getFeaturedDish().subscribe( dish => this.dish = dish,errmess => this.dishErrMess = <any>errmess);
+    this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion,errmess => this.promotionErrMess = <any>errmess);
+    this.leaderservice.getFeaturedLeader().subscribe(leader => this.leader = leader,errmess => this.leaderErrMess = <any>errmess);
+  }
+*/
 
 }
 
